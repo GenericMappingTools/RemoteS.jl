5a0868bef72cbf5b47ef175fd83fdedc9e7d3bed
Joaquim Luis <jmfluis@gmail.com>
2021-08-04 11:57:26 +0100

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,6 @@
-using RemoteS
-using Test
+using RemoteS, GMT
+using Test, Printf
+
+truecolor(mat2img(rand(UInt16,128,128)), mat2img(rand(UInt16,128,128)), mat2img(rand(UInt16,128,128)));
+@test guess_increment_from_coordvecs([1., 1, 1, 1], [1., 1, 1, 1]) == 1.0
+@test helper_find_sds("AA", "xxxxxxxx:AA", findall("\n", @sprintf("aA\nbbbbbnnn\n"))) == "xxx:AA"
