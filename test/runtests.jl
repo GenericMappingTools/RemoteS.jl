8432e00516db63fb32da233a2b4f805bf6b9dd66
Joaquim Luis <jmfluis@gmail.com>
2021-09-05 00:04:45 +0100

    Add the TLE file name to read as a kwarg. Fix silly return of only one GMTdatset when it was a vector (tiles case)

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,6 @@ truecolor(mat2img(rand(UInt16,128,128)), mat2img(rand(UInt16,128,128)), mat2img(
 
 truecolor(mat2img(rand(UInt16, 16, 16, 3), noconv=1), [1,2,3]);
 truecolor(mat2img(rand(UInt16, 16, 16), noconv=1), mat2img(rand(UInt16, 16, 16), noconv=1), mat2img(rand(UInt16, 16, 16), noconv=1));
+
+sat_tracks(position=true);
+sat_tracks(position=true, tle=["1 27424U 02022A   21245.83760660  .00000135  00000-0  39999-4 0  9997"; "2 27424  98.2123 186.0654 0002229  67.6025 313.3829 14.57107527 28342"]);
