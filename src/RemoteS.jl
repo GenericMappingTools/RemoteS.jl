0194c44da86e63a97690fc50c73e2e136a271540
Joaquim Luis <jmfluis@gmail.com>
2021-09-08 02:28:29 +0100

    Add the findscenes function to search for scenes close to a location.

--- src/RemoteS.jl
+++ src/RemoteS.jl
@@ -2,6 +2,8 @@ module RemoteS
 
 using GMT, Printf, Requires, Dates
 
+const SCENE_HALFW = Dict("AQUA" => 1163479, "TERRA" => 1163479, "LANDSAT8" => 95236)	# half widths
+
 if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
 	@eval Base.Experimental.@optlevel 1
 end
@@ -10,7 +12,7 @@ export
 	dn2temperature, dn2radiance, dn2reflectance, reflectance_surf, grid_at_sensor, truecolor,
 	clg, clre, evi, evi2, gndvi, mndwi, mtci, mcari, msavi, mbri, ndvi, ndwi, ndwi2, ndrei1,
 	ndrei2, satvi, savi, slavi,
-	sat_tracks, findscene
+	sat_tracks, findscenes
 
 include("grid_at_sensor.jl")
 include("utils.jl")
