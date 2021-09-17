7b4149253f4191c71b30c8df8251800e78230dc6
Joaquim Luis <jmfluis@gmail.com>
2021-09-17 14:02:29 +0100

    Let the Landsat8 files get the description automatically

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,9 @@ truecolor(mat2img(rand(UInt16,128,128)), mat2img(rand(UInt16,128,128)), mat2img(
 truecolor(mat2img(rand(UInt16, 16, 16, 3), noconv=1), [1,2,3]);
 truecolor(mat2img(rand(UInt16, 16, 16), noconv=1), mat2img(rand(UInt16, 16, 16), noconv=1), mat2img(rand(UInt16, 16, 16), noconv=1));
 
+@test RemoteS.assign_description(["LC08_B2.TIF", "LC08_B5.TIF"], String[])[1] == "Band 2 - Blue [0.45-0.51]"
+@test RemoteS.assign_description(["AC08_B2.TIF", "AC08_B5.TIF"], String[])  == ["AC08_B2", "AC08_B5"]
+@test RemoteS.assign_description(["AC08_B2.TIF", "AC08_B5.TIF"], ["B2", "B5"]) == ["B2", "B5"]
+
 #sat_tracks(position=true);
 #sat_tracks(position=true, tle=["1 27424U 02022A   21245.83760660  .00000135  00000-0  39999-4 0  9997"; "2 27424  98.2123 186.0654 0002229  67.6025 313.3829 14.57107527 28342"]);
